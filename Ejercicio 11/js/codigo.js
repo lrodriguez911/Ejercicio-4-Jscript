2e2d2006fcab4861456af8e9ade272cfc6f385b7
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2021-10-12 14:44:59 -0300

    ejercicio once completo

--- Ejercicio 11/js/codigo.js
+++ Ejercicio 11/js/codigo.js
@@ -1,4 +1,5 @@
 let numero = parseInt(prompt('ingrese un numero'));
-if (numero % 2) {
-    
-}
+numero % 2 === 0 ? document.write('es divisible por 2' + '<br>') : '';
+numero % 3 === 0 ? document.write('es divisible por 3' + '<br>') : '';
+numero % 5 === 0 ? document.write('es divisible por 5' + '<br>') : '';
+numero % 7 === 0 ? document.write('es divisible por 7' + '<br>') : '';
