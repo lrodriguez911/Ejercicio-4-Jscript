0a734b0a78d6aa0f58c73374a188f699cf93d1ed
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-07-23 22:36:02 -0300

    using async await

--- API rest/codigo.js
+++ API rest/codigo.js
@@ -4,3 +4,9 @@ const URL = 'https://jsonplaceholder.typicode.com/users';
 fetch(URL)
 .then(e => e.json())
 .then(ejason => console.log(ejason.map(e => e.name)))
+
+//using async await
+const async fetchApi = (URL) => {
+    await console.log('hola')
+}
+fetchApi()
